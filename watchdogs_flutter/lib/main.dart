5cb497865ea12456f10b995f4ca0d329cfbfc626
NagashL0l <metridxblack@gmail.com>
2019-12-11 17:11:16 +0100

    Imagen de fondo

--- watchdogs_flutter/lib/main.dart
+++ watchdogs_flutter/lib/main.dart
@@ -1,20 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+const ColorAppbar = const Color(0xFFFF00);
+//const PaddingGeneral = const Padding(padding: 32);
+
+
+
+void main() {
+
+  runApp(MyApp());
+
+}
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'WatchDogs',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Center(
+            child: Text('WatchDogs'),
+          )
         ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+        body: new Stack(
+          children: <Widget>[
+            new Container(
+              decoration: new BoxDecoration(
+                image: new DecorationImage(image: new AssetImage("images/warningDog.png"), fit: BoxFit.cover,),
+              ),
+            ),
+            new Container(
+              child: Text("Pantalla principal"),
+            )
+          ],
+        )
       ),
     );
   }
+
+  @override
+  Widget OpcionesPantallaPrincipal(BuildContext context) {
+    return MaterialApp(
+      padding : const EdgeInsets.all(32),
+      child: Container(
+        children: <Widget> [
+
+        ]
+
+      )
+
+    );
+
+
+
+  }
+
 }
